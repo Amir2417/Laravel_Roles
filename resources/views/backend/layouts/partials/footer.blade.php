4cc9c40a1365c7e49956472abd3e898e53fffeac
Amir2417 <amirsohelto@gmail.com>
2022-07-04 23:01:45 +0600

    Admin Panel Set Up

---
+++ resources/views/backend/layouts/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="footer-area">
+        <p>© Copyright 2022. All right reserved. Template by <a href="https://pigeonsoft.com">Pigeon Soft</a>.</p>
+    </div>
+</footer>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +20,14 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+/*
+
+Admin
+*/
+Route::prefix('admin')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('admin.dashboard');
+
+
+});
